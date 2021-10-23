426c09a407f155d836f8d674c1a0814a42dd8c0e
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-10-23 11:41:16 +0530

    Added change in Assignment on flip box in Handling multiple events

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,70 +1,25 @@
-let root = document.querySelector(".wrapper");
-let ul = document.createElement("ul");
-ul.classList.add("boxes");
-
-root.append(ul);
-
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 
-function getRandomNumber(){
-    return Math.floor(Math.random() * 12)
-}
+let firstBox = document.querySelectorAll(".first li");
 
+firstBox.forEach((box, index) =>{
+    box.addEventListener("click", function(event){
+        event.target.innerText = index + 1;
 
-numbers.forEach((number) => {
-     // Create 12 boxes
-    let li = document.createElement("li");
-    li.classList.add("box");
-
-    // On event get number on box
-
-    function handleClick(){
-        li.innerText = getRandomNumber();
-
-        // Remove the content after 5 seconds
         setTimeout(() => {
-            li.innerText = "";
+            event.target.innerText = "";
         }, 5000);
-    }
-
-    li.addEventListener("click", handleClick)
-
-    ul.append(li);
+    })
 })
 
-ul = document.createElement("ul");
-ul.classList.add("boxes");
+let secondBox = document.querySelector(".second");
 
-root.append(ul);
+secondBox.addEventListener("click", function(event){
+    console.dir(event.target.dataset.text)
+    let text = event.target.dataset.text;
+    event.target.innerText = text
 
-
-
-
-//// Event with deligation
-
-
-
-
-function handleClick(event){
-    event.target.innerText = getRandomNumber();
-
-   // Remove the content after 5 seconds
     setTimeout(() => {
         event.target.innerText = "";
     }, 5000);
-}
-
-numbers.forEach((number) => {
-    // Create 12 boxes
-    let li = document.createElement("li");
-    li.classList.add("box");
-    li.setAttribute("data-class", number)
-    ul.append(li);   
 })
-
-
-// On click get Number on box
-ul.addEventListener("click", handleClick);
-
-
