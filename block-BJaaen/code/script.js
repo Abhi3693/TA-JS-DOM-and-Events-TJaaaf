47492bac75e322c1e5c0f9d75832f3bcb51de23f
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-10-25 15:42:39 +0530

    Added change in assignment of  Rock Paper ans Scissor in DOM and Events

--- block-BJaaen/code/script.js
+++ block-BJaaen/code/script.js
@@ -1,42 +1,90 @@
-let userIcons = document.querySelectorAll(".user");
-let computerIcons = document.querySelectorAll(".computer");
-let userScore = document.querySelector(".addUserScore");
-let computerScore = document.querySelector(".addcomputerScore");
+let userIcons = document.querySelector(".user-icons");
+let computerIcons = document.querySelector(".computer-icons");
+
+let userScore = document.querySelector(".user-Score");
+let computerScore = document.querySelector(".computer-score");
 let display = document.querySelector(".display");
 let reset = document.querySelector(".reset"); 
- 
+
+let dataset = [
+    {
+        name: "rock",
+        Description: "Rock crushes Scissors"
+    },
+    {
+        name: "paper",
+        Description: "Paper covers Rock"
+    },
+    {
+        name: "scissors",
+        Description: "Scissors cuts Paper"
+    }
+]
+
+function createUserIcon(){
+    dataset.forEach((item) => {
+        let li = document.createElement("li");
+        let i = document.createElement("i");
+        i.className = (`fa fa-hand-${item.name}-o`)
+        li.append(i);
+        userIcons.append(li);
+    })
+}
+createUserIcon();
+
+function createComputerIcon(){
+    dataset.forEach((item) => {
+        let li = document.createElement("li");
+        let i = document.createElement("i");
+        i.className = (`fa fa-hand-${item.name}-o`);
+        li.append(i);
+        computerIcons.append(li);
+    })
+}
+createComputerIcon();
+
 function getRandomNumber(){
     return Math.floor(Math.random() * 3)
 }
 
-userIcons.forEach((icon) => {
+let userSelect = userIcons.querySelectorAll("i")
+let computerSelect = computerIcons.querySelectorAll("i")
+
+function blackIcons(){
+    userSelect.forEach((icon) =>{
+        icon.style.color = "black";
+    })
+    computerSelect.forEach((icon) =>{
+        icon.style.color = "black";
+    })
+}
+
+userSelect.forEach((icon) => {
+
     function handleClick(event) {
         
-        userIcons[0].style.color = "black";
-        userIcons[1].style.color = "black";
-        userIcons[2].style.color = "black";
-
+        blackIcons();
         event.target.style.color = "blue";
 
-        let user = event.target.dataset.type;
-        
+        let user = event.srcElement.classList[1];
+    
         let computerOptionIndex = getRandomNumber();
-        let computer = computerIcons[computerOptionIndex].dataset.type;
 
-        computerIcons[0].style.color = "black";
-        computerIcons[1].style.color = "black";
-        computerIcons[2].style.color = "black";
+        let computer = computerSelect[computerOptionIndex].classList[1];
 
-        computerIcons[computerOptionIndex].style.color = "red";
+        computerSelect[computerOptionIndex].style.color = "red";
 
         if(user === computer) {
-            display.innerText = "It's a tie"
+            display.style.color = "grey";
+            display.innerText = "It's a tie";
 
-        } else if(user === "Paper" && computer === "Rock" || user === "Rock" && computer === "Scissors"){
-            display.innerText = "You win";
+        } else if(user === "fa-hand-paper-o" && computer === "fa-hand-rock-o" || user === "fa-hand-rock-o" && computer === "fa-hand-scissors-o"){
+            display.style.color = "green";
+            display.innerText = "You Win!!!";
             userScore.innerText =  +(userScore.innerText) + 1;
         } else {
-            display.innerText = "Computer win"
+            display.style.color = "red";
+            display.innerText = "Computer Win";
             computerScore.innerText =  +(computerScore.innerText) + 1;
         }
     }
@@ -47,10 +95,8 @@ reset.addEventListener("click", function(){
     userScore.innerText = 0;
     computerScore.innerText = 0;
     display.innerText = "";
-    userIcons[0].style.color = "black";
-    userIcons[1].style.color = "black";
-    userIcons[2].style.color = "black";
-    computerIcons[0].style.color = "black";
-    computerIcons[1].style.color = "black";
-    computerIcons[2].style.color = "black";
+    blackIcons();
 })
+
+computerScore.style.color = "red";
+userScore.style.color = "green";
