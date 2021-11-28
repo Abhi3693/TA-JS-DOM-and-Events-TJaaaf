86ea5c871fca9885abcc807b3ef554b7119fe17d
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-11-28 10:51:24 +0530

    add change

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,225 +1,204 @@
-
-let allArray = 
-[
+let cardData = [
     {
-        name: "key",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" /></svg>`,
-
+        name: "glob",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
+        display: false
     },
     {
-        name: "hashTag",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" /></svg>`,
-
+        name: "home",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>`,
+        display: false
     },
     {
-        name: "play",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" /></svg>`,
-
-    },
-    {
-        name: "camera",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" /></svg>`,
-
+        name: "hashtag",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" /></svg>`,
+        display: false
     },
     {
         name: "hand",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" /></svg>`,
-
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" /></svg>`,
+        display: false
     },
     {
-        name: "gift",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" /></svg>`,
-
+        name: "heart",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>`,
+        display: false
     },
     {
-        name: "library",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" /></svg>`,
-
+        name: "flag",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" /></svg>`,
+        display: false
     },
     {
-        name: "lock",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>`,
-
-    },
-
-    {
-        name: "key",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" /></svg>`,
-
+        name: "eye",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>`,
+        display: false
     },
     {
-        name: "hashTag",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" /></svg>`,
-
+        name: "happy",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
+        display: false
     },
     {
-        name: "play",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" /></svg>`,
-
+        name: "glob",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
+        display: false
     },
     {
-        name: "camera",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" /></svg>`,
-
+        name: "home",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>`,
+        display: false
+    },
+    {
+        name: "hashtag",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" /></svg>`,
+        display: false
     },
     {
         name: "hand",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" /></svg>`,
-
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" /></svg>`,
+        display: false
     },
     {
-        name: "gift",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" /></svg>`,
-
+        name: "heart",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>`,
+        display: false
     },
     {
-        name: "library",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" /></svg>`,
-
+        name: "flag",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" /></svg>`,
+        display: false
     },
     {
-        name: "lock",
-        display: false,
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" /></svg>`,
-
-    }
-];
-
+        name: "eye",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" /></svg>`,
+        display: false
+    },
+    {
+        name: "happy",
+        path: `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`,
+        display: false
+    },
+]
 
 let rootElm = document.querySelector(".cardHolder");
-
-allArray.sort(() => 0.5 - Math.random());
-
-let count = document.querySelector("small");
-    count.innerText = 0;
-
-let timer = document.querySelector(".addTime");
-timer.innerText = 0;
-
-let shuffleButton = document.querySelector(".reset");
-
-function handleShuffle() {
-
-    allArray.sort(() => 0.5 - Math.random());
-
-    allArray.forEach((val) => {
-        val.display = false;
-    })
-    match = [];
-    getMatch();
-    createCards();
-    count.innerText  = 0;
-    timer.innerText = 0;
-}
-shuffleButton.addEventListener("click", handleShuffle)
-
-
-function createCards() {
-    rootElm.innerHTML = "";
-
-    allArray.forEach((item,index) => {
-
-        let div = document.createElement("div");
-        div.classList.add("card");
-        div.setAttribute("data-name", item.name);
-        div.setAttribute("data-id", index);
-        div.setAttribute("data-isSelected", item.display);
-    
-        if(item.display === false){
-            div.style.backgroundColor = "pink"
-        } else {
-            div.innerHTML =   allArray[index].icon;
-        }     
-        rootElm.append(div);
-    })
-}  
-
-function hideValue() {
-    allArray.forEach((val) => {
-        val.display = false;
-    })
-
-    match = [];
-    getMatch()
-
-    createCards();
-}
+let reset = document.querySelector("h2");
+let count = document.querySelector(".count");
+let timer = document.querySelector(".timer");
 
 let match = [];
+let matchedCards = [];
+let firstClick = [];
 
-let matchedCard = [];
+let countStep = 0;
+let time ;
+
+count.innerText = `Count : 0`;
+timer.innerText = 0;
+
+cardData.sort(() => 0.5 - Math.random());
+
+function hideValue() {
+    cardData.forEach((card) => {card.display = false});
+    match = [];
+    renderCards();
+    getMatch();
+    renderCards();
+}
 
 function getMatch() {
 
     if (match.length === 2) {
-                    
-        if(match[0].name === match[1].name){
 
-            console.log("matched");
+        if(match[0].name === match[1].name) {      
+            matchedCards.push(match[0],match[1]);
             match = [];
-            matchedCard.push(match[0],match[1]);
             getResult();
-            count.innerText = Number(count.innerText) + 1;
-            } else {
-                let id1 = match[0].id;
-                let id2 = match[1].id;
-
-                matchedCard = [];
-                setTimeout(hideValue, 500)
-                console.log("not matched")
-                match = [];
-                createCards();
-                count.innerText = Number(count.innerText) + 1;
+            getMatch();
+            countStep = Number(countStep) + 1; 
+            count.innerText = `Count : ${countStep}`; 
+        } else{
+            matchedCards = [];
+            match = [];
+            setTimeout(hideValue, 500);
+            renderCards();
+            countStep = Number(countStep) + 1;
+            count.innerText = `Count : ${countStep}`; 
         }
     }
+}
+
+function handleShuffle() {
+    cardData.sort(() => 0.5 - Math.random());
+    cardData.forEach((val) => {
+        val.display = false;
+    })
+    match = [];
+    count.innerText = `Count : 0`; 
+    firstClick = [];
+    timer.innerText = 0;
+    renderCards();
+    getMatch();
+    clearInterval(time);
+}
+
+reset.addEventListener("click", handleShuffle)
+
+function getResult() {
+    if(matchedCards.length === cardData.length) {
+        setTimeout( ()=> {
+            alert(`You won! Time required to win is ${timer.innerText}`);
+            timer.innerText = 0;
+            handleShuffle();
+            getFirstClick();
+        },1000)
+     }
 }
 
 function getTimer() {
     timer.innerText = Number(timer.innerText) + 1;
 }
 
-const handleClick = (event) => {
-    
-    let id = event.target.dataset.id;
-
-    let value = event.target.dataset;
-
-    allArray[id].display = !allArray[id].display;
-
-    match.push(value);
-
-    getMatch();
-    
-    createCards();  
-}
-
-createCards();
-
-function getResult() {
-
-    if(matchedCard.length === allArray.length){
-        handleShuffle();
-        alert("You Won");        
-        alert(`Required time to complete game is ${timer.innerText}`);
+function getFirstClick() {
+    if(firstClick.length === 1) {
+         time = setInterval(getTimer, 1000);
+    } else if(matchedCards.length === cardData.length){
+        clearInterval(time);      
     }
 }
 
-setInterval(getTimer, 1000);
+function renderCards() {
 
-rootElm.addEventListener("click", handleClick);
+    rootElm.innerHTML = "";
 
+    cardData.forEach((card, index)=> {
+
+        let div = document.createElement("div");
+        div.classList.add("card");
+        div.setAttribute("data-name", card.name);
+        div.setAttribute("data-isDisplay", card.display);
+        div.setAttribute("data-path", card.path);
+        div.setAttribute("data-id", index);
+        
+        div.addEventListener("click", (event)=> {
+            let val = event.target.dataset;
+            let id = val.id;
+            cardData[id].display = true;
+            match.push(val);
+
+            renderCards();
+            getMatch();
+
+            firstClick.push(id);
+            getFirstClick();
+        })
+
+        if(card.display === false) {
+            div.style.backgroundColor = "bisque";
+        } else{
+            div.innerHTML = card.path;
+        }
+        rootElm.append(div);
+    })
+}
+renderCards();
